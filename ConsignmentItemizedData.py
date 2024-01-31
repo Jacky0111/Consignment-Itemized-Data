8672d77a712ddeb430b127ad04ba61f37fc5189c
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2024-01-31 17:58:54 +0800

    Create ConsignmentItemizedData.py

---
+++ ConsignmentItemizedData.py
@@ -0,0 +1,13 @@
+# Empowers the end user to make choices between performing document conversion and optical character recognition (OCR)
+
+import os
+import cv2
+import shutil
+from datetime import datetime
+
+from Detect import Detect
+from OpticalCharacterRecognition import OCR
+
+class CID:
+    def __init__(self):
+        pass
