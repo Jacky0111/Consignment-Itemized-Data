3fedb108994dc7096776f782c667e3e982017d11
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2024-02-05 10:24:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
 import sys
-import tempfile
-import webbrowser
 import pandas as pd
-import streamlit as st
 from PIL import Image
+import streamlit as st
 from streamlit import runtime
 from streamlit.web import cli as stcli
 from pdf2image import convert_from_path
@@ -16,8 +14,6 @@ pd.set_option('display.max_columns', None)
 
 
 class App:
-    # df = pd.DataFrame()
-
     uploaded_file = None
 
     def __init__(self):
@@ -34,47 +30,18 @@ class App:
             # Convert PDF to images using pdf2image
             images = self.pdf_to_images(pdf_path)
 
-            # Display selected images in the Streamlit app
-            selected_pages = st.multiselect("Select pages to keep", [i + 1 for i in range(len(images))],
-                                            default=[i + 1 for i in range(len(images))])
-
-            # Mark selected pages in the backend
-            print("Selected pages:", selected_pages)
-
-            # Remove unselected pages
-            self.remove_unselected_images("output_images", selected_pages)
-
-            # Display selected images
-            selected_images = [images[i - 1] for i in selected_pages]
-
-            # Sort selected pages in ascending order
-            selected_pages.sort()
-
-            # Create placeholder for dynamic update
-            st.empty()
-
-            # Display selected images in rows and columns dynamically
-            num_columns = 5
-            for i in range(0, len(selected_images), num_columns):
-                row = st.columns(num_columns)
-                for j in range(num_columns):
-                    index = i + j
-                    if index < len(selected_images):
-                        si = selected_images[index]
-                        row[j].image(si, caption=[f"Page {selected_pages[index]}"])
 
             # Cleanup: Remove the temporary PDF file
             os.remove(pdf_path)
 
-            for img in selected_images:
-                cid = CID(img)
+            # for img in selected_images:
+            #     cid = CID(img)
 
     '''
     Set the title and page configuration for wider layout
     '''
     @staticmethod
     def header():
-        # st.set_page_config(page_title="PDF Viewer with Streamlit", layout="wide")
         st.write('# PDF Viewer with Streamlit')
 
     '''
@@ -82,6 +49,7 @@ class App:
     '''
     def uploadFile(self):
         self.uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+        print(f'self.uploaded_file: {self.uploaded_file}')
 
     '''
     Convert PDF to images using pdf2image
@@ -90,17 +58,7 @@ class App:
     def pdf_to_images(pdf_path):
         images = convert_from_path(pdf_path)
         return images
-
-    '''
-    Remove unselected pages
-    '''
-    @staticmethod
-    def remove_unselected_images(output_folder, selected_pages):
-        for i in range(1, len(selected_pages) + 1):
-            image_path = os.path.join(output_folder, f"output_image-{i}.png")
-            if i not in selected_pages and os.path.exists(image_path):
-                os.remove(image_path)
-
+    
 
 if __name__ == '__main__':
     if runtime.exists():
