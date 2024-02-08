65bbf41b01ac9e3d7b9536f7d053b2ba4048a8c6
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2024-02-08 12:09:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,65 +14,33 @@ from ConsignmentItemizedData import CID
 st.set_page_config(layout="wide")
 pd.set_option('display.max_columns', None)
 
-
 class App:
     uploaded_files = None
 
     def __init__(self):
-        App.header()
-        self.uploadFile()
-
-    def runner(self):
-        if hasattr(self, 'uploaded_files') and self.uploaded_files:
-            for file in self.uploaded_files:
-                # Save the uploaded PDF file to a temporary location
-                pdf_path = f'data/temp/{file.name}'
-
-                try:
-                    with open(pdf_path, "wb") as f:
-                        f.write(file.read())
-                    st.success(f"File '{file.name}' has been successfully uploaded.")
-
-                except (FileNotFoundError, FileExistsError):
-                    os.makedirs('data/temp/', exist_ok=True)
-
-            # Convert PDF to images using pdf2image
-            # images = self.pdf_to_images(pdf_path)
-            #
-            # # Cleanup: Remove the temporary PDF file
-            # os.remove(pdf_path)
-            #
-            # # for img in selected_images:
-            #     cid = CID(img)
+        pass
 
     '''
     Set the title and page configuration for wider layout
     '''
-    @staticmethod
-    def header():
+    def header(self):
         st.write('# Consignment Itemized Data')
 
     '''
     Upload pdf file
     '''
     def uploadFile(self):
-        self.uploaded_files = st.file_uploader("Upload PDF files", type="pdf", accept_multiple_files=True)
-        print(f'self.uploaded_files: {self.uploaded_files}')
+        pass
 
-    '''
-    Convert PDF to images using pdf2image
-    '''
-    @staticmethod
-    def pdf_to_images(pdf_path):
-        images = convert_from_path(pdf_path)
-        return images
 
+
+    
 
 if __name__ == '__main__':
     if runtime.exists():
         # If the runtime environment exists, create a Deployment object and start the runner
         dep = App()
-        dep.runner()
+
     else:
         # If the runtime environment doesn't exist, start the Streamlit application
         sys.argv = ['streamlit', 'run', 'app.py']
@@ -83,4 +51,3 @@ if __name__ == '__main__':
 # returns True if the environment is present and False otherwise. If the environment exists, a Deployment object is
 # created and its 'runner()' method is called. Otherwise, the Streamlit application is started using the 'sys.argv' and
 # 'stcli.main()' functions.
-
