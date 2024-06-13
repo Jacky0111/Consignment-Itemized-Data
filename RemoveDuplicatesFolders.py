0893be1a7c82cf8b2d027cfebaeb90943df3129f
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2024-06-14 00:02:57 +0800

    Update RemoveDuplicatesFolders.py

--- RemoveDuplicatesFolders.py
+++ RemoveDuplicatesFolders.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 # Define the parent folder path
-parent_folder_path = "D:/Archive\\Sample_451-500"
+parent_folder_path = "D:/Archive\\Sample_651-700"
 
 # Get the list of folders in the parent folder
 parent_folders = os.listdir(parent_folder_path)
